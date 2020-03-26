4baf4afe2c602d59f05d31c127fc167ad388f5b7
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-03-26 19:09:23 +0100

    update with working build system

--- Template/addContent.py
+++ Template/addContent.py
@@ -8,6 +8,7 @@
 import sys
 import getopt
 import glob
+import re
 
 
 def main(argv):
@@ -30,39 +31,78 @@ def main(argv):
     if inputfile == '' or outputfile == '':
         print('addContent.py -i <main.tex> -o <main_with_content.tex>')
         sys.exit(2)
-    print("Input file is \"%s\"" % (inputfile))
-    print("Output file is \"%s\"" % (outputfile))
+    print("Start processing")
+    print("  Input file is \"%s\"" % (inputfile))
+    print("  Output file is \"%s\"" % (outputfile))
+    print("")
 
     # read the main template
     main_tex = ''
     with open(inputfile, 'r') as reader:
         main_tex = reader.read()
+    if main_tex != '':
+        print("read main template done...")
+    else:
+        print("Error: no %s file found." % (inputfile))
+        sys.exit(-1)
 
     # we replace the following placeholders
     replacements = {'%%PLACEHOLDERSOFTWARE%%': 'Software',
-                    '%%PLACEHOLDEREVENTS%%': 'Events'}
+                    '%%PLACEHOLDEREVENTS%%': 'Events',
+                    '%%PLACEHOLDERDISSERTATIONS%%': 'Dissertations',
+                    '%%PLACEHOLDERPROJECTS%%': 'Projects'
+                    }
 
+    c = 0
     for replacement in replacements:
+        print("Replacement %d" % (c))
+        c = c + 1
         text = ''
         key = replacement
         folder = replacements[key]
-        print("replace %s with content from folder %s" % (key, folder))
+        print("  replace %s with content from folder %s" % (key, folder))
         template = ''
         with open("pieces/%s.temp" % (folder)) as reader:
             template = reader.readlines()
+        template = "".join(template)
 
         # now look for content
         files = glob.glob("../%s/*/*.md" % (folder))
         for file in files:
-            print("parse the content in %s" % file)
+            print("  parse the content in %s" % file)
+            file_content = ''
+            with open(file, 'r') as f:
+                file_content = f.read()
+
             # add the parsed fields to the template
             thistext = template
+            # what are the keys in the template
+            m = re.findall('%%[a-zA-Z0-9]+%%', thistext)
+            d = {}
+            for n in m:
+                n_base_text = n.replace("%", "")
+                d[n_base_text] = ''
+                # check if we have a value in file_content
+                if n_base_text == "text":
+                    # special case, we take everything in the file that comes after
+                    s = re.search("text:(.*)$", file_content,
+                                  re.MULTILINE | re.DOTALL)
+                else:
+                    s = re.search(
+                        n_base_text + ": ([^\n]+)", file_content, re.IGNORECASE)
+                try:
+                    d[n_base_text] = s.group(1)
+                except:
+                    pass
 
+            # now replace in thistext all the occurences of our pattern
+            for k, v in d.items():
+                thistext = thistext.replace("%%%%%s%%%%" % k, v)
             # add the template at the end of 'text'
             text = "%s\n%s" % (text, thistext)
 
         # now replace key in main_tex with 'text'
-        main_tex.replace(key, text)
+        main_tex = main_tex.replace(key, text)
 
     # now save the new main_tex to disc again
     with open(outputfile, 'w') as writer:
